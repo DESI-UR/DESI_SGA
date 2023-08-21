a30468fd2e88d80f471f24dc55da53db8b9184a4
kadglass <kad347@drexel.edu>
2023-08-21 15:18:53 -0700

    Final versions of figures for paper

--- TF/SV/help_functions.py
+++ TF/SV/help_functions.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+import matplotlib.colors as mc
+
+import colorsys
+
 
 
 ################################################################################
@@ -22,5 +26,38 @@ def firstdigit(n):
     digit = np.trunc(n * 10**(-np.trunc(np.log10(n)))).astype(int)
     
     return digit
+################################################################################
+################################################################################
+
+
+
+
+################################################################################
+#-------------------------------------------------------------------------------
+def adjust_lightness(color, amount=0.5):
+    '''
+    Change the shade of the color.
+    
+    
+    PARAMETERS
+    ==========
+    
+    color : string or length-3 tuple
+        Matplotlib color name OR rgb tuple
+    
+    amount : float
+        Amount by which to lighten or darken the color.  Values < 1 will 
+        return a darker shade, while values > 1 will return a lighter shade.  
+        Default is 0.5.
+    '''
+    
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    
+    return colorsys.hls_to_rgb(c[0], max(0, min(1, amount*c[1])), c[2])
 ################################################################################
 ################################################################################
