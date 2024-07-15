Fixed peculiar velocity calculation

TF/pv_calc.py
@@ -10,6 +10,7 @@ import numpy as np
 
 from astropy.table import Table
 import astropy.constants as const
+import astropy.units as u
 ################################################################################
 
 
@@ -17,6 +18,9 @@ import astropy.constants as const
 ################################################################################
 # Constants
 #-------------------------------------------------------------------------------
+h = 1
+H0 = 100*h*u.km/u.s/u.Mpc
+
 c = const.c.to('km/s')
 
 # Number of random samples to generate for uncertainties
@@ -32,13 +36,64 @@ rng = np.random.default_rng()
 #-------------------------------------------------------------------------------
 data_directory = 'SV/'
 
-filename = 'SGA_fuji_ITFR_moduli.fits'
+# filename = 'SGA_fuji_ITFR_moduli.fits'
+filename = 'SGA_fuji_jointTFR-varyV0-perpdwarf_moduli.fits'
 
 galaxies = Table.read(data_directory + filename)
 ################################################################################
 
 
 
+################################################################################
+# Compute peculiar velocities
+#-------------------------------------------------------------------------------
+def dist(mu):
+    '''
+    Compute the distance to an object based on a distance modulus.
+    
+    
+    PARAMETERS
+    ==========
+    mu : float or ndarray of shape (n,)
+        Distance moduli
+    
+    RETURNS
+    =======
+    d : float or ndarray of shape (n,)
+        Distance in units of Mpc (or Mpc/h, depending on the units of the 
+        distance modulus)
+    '''
+    d = 10 * 10**(0.2*mu) # pc or pc/h
+    return d/1e6 # Mpc or Mpc/h
+
+
+gal_d = dist(galaxies['mu_TFbright']) # Mpc/h
+
+galaxies['V_PEC'] = c*galaxies['Z_DESI'] - H0*(gal_d*u.Mpc) # km/s
+#-------------------------------------------------------------------------------
+# Estimate uncertainty
+#-------------------------------------------------------------------------------
+galaxies['VERR_PEC'] = np.nan
+
+for i in range(len(galaxies)):
+    
+    z_desi_random = rng.normal(galaxies['Z_DESI'][i], 
+                               galaxies['ZERR_DESI'][i], 
+                               size=N_samples)
+    mu_random = rng.normal(galaxies['mu_TFbright'][i], 
+                           galaxies['mu_TFbright_err'][i], 
+                           size=N_samples)
+    
+    d_random = dist(mu_random)
+    
+    Vpec_random = c*z_desi_random - H0*(d_random*u.Mpc)
+    
+    galaxies['VERR_PEC'][i] = np.nanstd(Vpec_random.data)
+#-------------------------------------------------------------------------------
+################################################################################
+
+
+"""
 ################################################################################
 # Compute cosmological redshift
 #-------------------------------------------------------------------------------
@@ -139,7 +194,7 @@ for i in range(len(galaxies)):
 galaxies['VERR_PEC'] = c*galaxies['ZERR_PEC']
 #-------------------------------------------------------------------------------
 ################################################################################
-
+"""
 
 
 ################################################################################