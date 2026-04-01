a87036a590b3d5b75254f272aa6fc5deda6f6908
Kelly Douglass <kellyadouglass@rochester.edu>
2026-04-01 11:31:03 -0400

    Updated example code for Fig. 10 (calibrated EDR TFR)

--- TF/SV/paper_figures/Fig10/fig10_data.py
+++ TF/SV/paper_figures/Fig10/fig10_data.py
@@ -14,7 +14,8 @@ from astropy.table import Table
 ################################################################################
 # Read in full catalog
 #-------------------------------------------------------------------------------
-hdul = fits.open('../../SGA_fuji_jointTFR-varyV0-perpdwarf-zCMB_dVsys_moduli.fits')
+# hdul = fits.open('../../SGA_fuji_jointTFR-varyV0-perpdwarf-zCMB_dVsys_moduli.fits')
+hdul = fits.open('../../SGA_fuji_jointTFR-varyV0-perpdwarf-zCMB_dVsys_corr_moduli-20260114.fits')
 
 gals = Table(hdul[1].data)
 
@@ -35,12 +36,14 @@ hdr['FIGURE'] = 10
 
 empty_primary = fits.PrimaryHDU(header=hdr)
 #-------------------------------------------------------------------------------
-table_hdu = fits.BinTableHDU(data=gals['Z_DESI_CMB', 'ZERR_DESI', 'LOGDIST', 'LOGDIST_ERR', 'DWARF'])
+table_hdu = fits.BinTableHDU(data=gals['V_0p33R26', 'V_0p33R26_ERR', 'R_ABSMAG_SB26_CORR', 'R_ABSMAG_SB26_CORR_ERR', 'DWARF'])
 
-table_hdu.columns['Z_DESI_CMB'].name = 'Z'
-table_hdu.columns['ZERR_DESI'].name = 'Z_ERR'
+table_hdu.columns['V_0p33R26'].name = 'VROT'
+table_hdu.columns['V_0p33R26_ERR'].name = 'VROT_ERR'
+table_hdu.columns['R_ABSMAG_SB26_CORR'].name = 'R_ABSMAG'
+table_hdu.columns['R_ABSMAG_SB26_CORR_ERR'].name = 'R_ABSMAG_ERR'
 
 hdul = fits.HDUList([empty_primary, table_hdu])
 
-hdul.writeto('fig10_data.fits')
+hdul.writeto('fig10_data.fits', overwrite=True)
 ################################################################################
